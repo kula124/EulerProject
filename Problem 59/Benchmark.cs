b2707b33beb0f396c396b0477289bf8e582ae3a2
Ivan <kula124@gmail.com>
2017-10-21 13:09:46 +0200

    Add project files.

---
+++ Problem 59/Benchmark.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProject
+{
+    static class Benchmark
+    {
+        private static Stopwatch a = new Stopwatch();
+        public static void Start()
+        {
+            a.Start();
+        }
+        public static void Stop()
+        {
+            if (a.IsRunning)
+                a.Stop();
+        }
+        public static string ShowTime()
+        {
+            return a.IsRunning ? null : a.ElapsedMilliseconds.ToString();
+        }
+    }
+}
---
+++ Problem 59/Program.cs
@@ -0,0 +1,111 @@
+﻿using EulerProject;
+using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+namespace Problem59
+{
+    class Program
+    {
+        static bool UseHashes = true;
+        static string hash;
+        static string key;
+        static Random r = new Random();
+        static string solution;
+        static Hashtable ht = new Hashtable();
+        static int c = 0;
+        static int ac = 0;
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("file.txt"))
+            {
+                hash = reader.ReadToEnd();
+            }
+            var words = hash.Split(',');
+            Benchmark.Start();
+            do
+            {
+                key = GenerateKey();
+                Console.Write("Tring key: ");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine(key);
+                Console.ResetColor();
+                c++;
+                solution = Decrypt(words);
+            }
+            while (!IsValid(solution, new string[] {" the ","and", " a "}));
+            Benchmark.Stop();
+            Console.Write("SOLVED with KEY: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(key);
+            Console.ResetColor();
+            Console.WriteLine("Keys tested: {0}", c);
+            Console.WriteLine("Key found in {0} ms", Benchmark.ShowTime());
+            Console.WriteLine(solution);
+            Console.WriteLine("CheckSum(solution): {0}", GetSum());
+            Console.ReadKey();
+        }
+
+        private static int GetSum()
+        {
+            int sum = 0;
+            foreach (var item in solution)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        private static string Decrypt(string[] words)
+        {
+            int i = 0;
+            StringBuilder sb = new StringBuilder();
+            foreach (var item in words)
+            {
+                var target = Int32.Parse(item);
+                var keyp = (int)key[i++];
+                var S = target ^ keyp;
+                sb.Append((char)S);
+                if (i == key.Length)
+                    i = 0;
+            }
+            return sb.ToString();
+        }
+        private static bool IsValid(string s, string[] words)
+        {
+            s = s.ToLower();
+            foreach (var item in words)
+            {
+                if (!s.Contains(item))
+                    return false;
+            }
+            return true;
+        }
+        private static string GenerateKey()
+        {
+            char[] t = new char[3];
+            for (int i = 0; i < 3; i++)
+            {
+                t[i] = (char)(r.Next(97, 122));
+            }
+            var key = new string(t);
+            ac++;
+            if (UseHashes)
+                if (KeyExistis(key))
+                    return GenerateKey();
+                else
+                    return key;
+            else
+                return key;
+        }
+
+        private static bool KeyExistis(string key)
+        {
+            if (!(ht[key] is null))
+                return true;
+            ht.Add(key, true);
+            return false;
+        }
+    }
+}
